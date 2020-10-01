f97a1a67320b10f895d5f5e68e9519196a0857f3
Blacwin <magyarbalint91@gmail.com>
2020-10-01 16:54:49 +0200

    update project view and the service layer

---
+++ src/test/java/com/maba/awesomeweather/AwesomeWeatherDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maba.awesomeweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeWeatherDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
